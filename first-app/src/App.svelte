1aa1fae6fb8839e4021430fc9524ba97581b7031
gaylonalfano <gaylon.alfano@gmail.com>
2019-10-02 21:55:41 +0800

    Initial commit. Adding first-app subfolder.

---
+++ first-app/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Gaylon";
+  let age = 30;
+
+  // let uppercaseName;  NOT required!
+
+  $: uppercaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === "Gaylon Alfano") {
+    age = 38;
+  }
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "Gaylon Alfano";
+  }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
+<ContactCard />
