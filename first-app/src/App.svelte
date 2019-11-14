385256041263a5e67de117979915a97665540d24
gaylonalfano <gaylon.alfano@gmail.com>
2019-11-14 19:08:47 +0800

    Wrapped up the section. Can't emphasize enough how cool the bind:value and $: features are. Next we're moving on to adding some more conditionals. Also learned about dynamically updating the CSS based on some condition using the class:some-css-class syntax. Cool stuff.

--- first-app/src/App.svelte
+++ first-app/src/App.svelte
@@ -9,34 +9,34 @@
 
   // let uppercaseName;  NOT required!
 
-  $: uppercaseName = name.toUpperCase();
+  // $: uppercaseName = name.toUpperCase();
 
-  $: console.log(name);
+  // $: console.log(name);
 
-  $: if (name === "Gaylon Alfano") {
-    age = 38;
-  }
+  // $: if (name === "Gaylon Alfano") {
+  //   age = 38;
+  // }
 
   function incrementAge() {
     age += 1;
   }
 
-  function changeName() {
-    name = "Gaylon Alfano";
-  }
+  // function changeName() {
+  //   name = "Gaylon Alfano";
+  // }
 
-  function nameInput(event) {
-    const enteredValue = event.target.value;
-    name = enteredValue;
-  }
-  function jobTitleInput(event) {
-    const enteredValue = event.target.value;
-    jobTitle = enteredValue;
-  }
-  function jobDescriptionInput(event) {
-    const enteredValue = event.target.value;
-    jobDescription = enteredValue;
-  }
+  // function nameInput(event) {
+  //   const enteredValue = event.target.value;
+  //   name = enteredValue;
+  // }
+  // function jobTitleInput(event) {
+  //   const enteredValue = event.target.value;
+  //   jobTitle = enteredValue;
+  // }
+  // function jobDescriptionInput(event) {
+  //   const enteredValue = event.target.value;
+  //   jobDescription = enteredValue;
+  // }
 </script>
 
 <style>
@@ -45,7 +45,7 @@
   }
 </style>
 
-<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<h1>Hello {name}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
 <!-- <button on:click={changeName}>Change Name</button> -->
 <!-- <input type="text" value={name} on:input={nameInput} /> -->
