80c0c516907c7371c9e173f216075f3a6a86b4f0
gaylonalfano <gaylon.alfano@gmail.com>
2019-11-17 12:05:36 +0800

    Created the initial project and really dove into different ways of mounting components and the difference between SPA and non-SPA approaches. Basically if we want to share data between components, then we embed. If we want totally isolated parts in our application, then we create root-level components (added directly to index.html).

---
+++ meetups-project/src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Header from "./global-components/Header.svelte";
+</script>
+
+<!-- <Header /> -->
+<Header />
---
+++ meetups-project/src/main.js
@@ -0,0 +1,16 @@
+import App from './App.svelte';
+// import Header from './global-components/Header.svelte';
+
+const app = new App({
+  target: document.body
+});
+
+// const app = new App({
+//   target: document.querySelector('#app')
+// });
+
+// const header = new Header({
+//   target: document.querySelector('#header')
+// });
+
+export default app;
