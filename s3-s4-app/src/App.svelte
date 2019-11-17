5bc0bf5326019c5080cd5ddfe27816b7f74610d6
gaylonalfano <gaylon.alfano@gmail.com>
2019-11-17 11:01:00 +0800

    Code used for exploring reactivity in Svelte, working with conditionals and loops, etc. One key aspect is to use the = sign to have Svelte update the DOM. This gets into the reference vs primitive values discussion. Next up is the course meetups project.

---
+++ s3-s4-app/src/App.svelte
@@ -0,0 +1,90 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formState = "empty";
+  let createdContacts = [];
+
+  function addContactCard() {
+    // Check if all items have content
+    if (
+      name.trim().length == 0 ||
+      title.trim().length == 0 ||
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+    createdContacts = [
+      ...createdContacts,
+      {
+        id: Math.random(),
+        name: name,
+        jobTitle: title,
+        imageURL: image,
+        jobDescription: description
+      }
+    ];
+    formState = "done";
+  }
+
+  function deleteFirst() {
+    // Return a new array starting at index 1 (0 is removed)
+    createdContacts = createdContacts.slice(1);
+  }
+  function deleteLast() {
+    // Return a new array starting at 0 and up to last element (-1)
+    createdContacts = createdContacts.slice(0, -1);
+  }
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContactCard}>Add Contact Card</button>
+<button on:click={deleteFirst}>Delete First</button>
+<button on:click={deleteLast}>Delete Last</button>
+
+{#if formState === 'invalid'}
+  <p>Invalid input.</p>
+{:else}
+  <p>Please enter some details and hit the button!</p>
+{/if}
+
+{#each createdContacts as contact, index (contact.id)}
+  <h2># {index + 1}</h2>
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    userImage={contact.imageURL}
+    description={contact.jobDescription} />
+{:else}
+  <p>Please start entering some contacts, we found none!</p>
+{/each}
