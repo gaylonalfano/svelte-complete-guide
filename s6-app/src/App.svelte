e9c8b2087ec37a5600c584f78dd8c624b37d4fd9
gaylonalfano <gaylon.alfano@gmail.com>
2019-11-24 12:54:13 +0800

    Learned about emitting custom events from child to parent using the createEventDispatcher function. Also learned about custom slot props and the special let: slotPropName syntax. These allow us to pass data from the component that has the slots, to the component that actually uses the slot. Pretty dense.

---
+++ s6-app/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
+
+  let products = [
+    {
+      id: "p1",
+      title: "A Book",
+      price: 9.99
+    },
+    {
+      id: "p2",
+      title: "A Watch",
+      price: 45.99
+    }
+  ];
+
+  let showModal = false;
+  let closeable = false;
+
+  function addToCart(event) {
+    console.log(event.detail);
+  }
+
+  function deleteProduct(event) {
+    console.log(event.detail);
+  }
+</script>
+
+{#each products as product}
+  <Product {...product} on:add-to-cart={addToCart} on:delete={deleteProduct} />
+{/each}
+
+<button on:click={() => (showModal = true)}>Show Modal</button>
+
+{#if showModal}
+  <Modal
+    on:cancel={() => (showModal = false)}
+    on:close={() => (showModal = false)}
+    let:didAgree={closeable}>
+    <h1 slot="header">Hello!</h1>
+    <p>This works! I should be placed where slot is at!</p>
+    <button
+      slot="footer"
+      on:click={() => (showModal = false)}
+      disabled={!closeable}>
+      Confirm
+    </button>
+  </Modal>
+{/if}
