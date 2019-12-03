7da125124b24421c96dd6d41b6c474985c547fc0
gaylonalfano <gaylon.alfano@gmail.com>
2019-12-03 09:35:13 +0800

    New module. Refresher on two-way binding and how to even two-way bind to custom component props. These are useful for form input elements, but are not recommended for two-way binding all of your components as that gets complicated and messy. Also note that two-way binding to a custom component input element type property is currently not an option in Svelte.

---
+++ s8-app/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import CustomInput from "./CustomInput.svelte";
+  import Toggle from "./Toggle.svelte";
+
+  let val = "Gaylon";
+  let selectedOption = 1;
+
+  $: console.log(val);
+  $: console.log(selectedOption);
+
+  function setValue(event) {
+    val = event.target.value;
+  }
+</script>
+
+<!-- Standard two-way binding -->
+<!-- <input type="text" value={val} on:input={setValue} /> -->
+
+<!-- Shortcut using bind: directive -->
+<!-- <input type="text" bind:value={val} /> -->
+
+<!-- Trying to bind to val property of CustomInput -->
+<CustomInput bind:val />
+<!-- Binding chosenOption to selectedOption -->
+<Toggle bind:chosenOption={selectedOption} />
